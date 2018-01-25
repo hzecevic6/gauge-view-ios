ee924d5467540b0c73ed30536ecb0bc1ba649c5c
Helena Zecevic <helena.zecevic@wolkabout.com>
2018-01-25 15:29:39 +0100

    Initial Commit

---
+++ GaugeTestiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GaugeTestiOS
+//
+//  Created by Helena Zecevic on 1/25/18.
+//  Copyright © 2018 Helena Zecevic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
